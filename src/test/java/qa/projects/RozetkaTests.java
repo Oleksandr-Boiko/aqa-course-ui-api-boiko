3aee24b870c8872813545acab2845c49f7f00287
Oleksandr Boiko <boiko.oleksandr@pdffiller.team>
2023-06-12 21:01:42 +0200

    fixes after pull review

--- src/test/java/qa/projects/RozetkaTests.java
+++ src/test/java/qa/projects/RozetkaTests.java
@@ -10,12 +10,14 @@ import pages.CartPage;
 import pages.MainPage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static pages.MainPage.checkListOfGoodsLoaded;
 
 public class RozetkaTests {
 
     private String iphone = "iphone";
     private String iphone13 = "iphone 13";
     private String apple = "Apple";
+    private String sortingValue = "2: expensive";
 
     @BeforeClass
     public void openSite() {
@@ -27,14 +29,14 @@ public class RozetkaTests {
         open(MainPage.baseURL);
     }
 
-    @Test
+    @Test(description = "Actions with cart")
     public void test1() {
         MainPage.cartButton.click();
         CartPage.cartModalName.shouldBe(Condition.text("Корзина"));
         CartPage.cartEmptyState.shouldBe(Condition.visible);
         CartPage.closeModalButton.click();
         MainPage.search(iphone);
-        MainPage.listOfGoods.shouldHave(CollectionCondition.size(30));
+        MainPage.listOfGoods.shouldHave(CollectionCondition.size(28));
         MainPage.buyButton.get(0).hover().click();
         MainPage.cartBadgeIcon.shouldBe(Condition.text("1"));
         MainPage.cartButton.click();
@@ -45,7 +47,7 @@ public class RozetkaTests {
         CartPage.cartEmptyState.shouldBe(Condition.visible);
     }
 
-    @Test
+    @Test(description = "Check list of categories")
     public void test2() {
         MainPage.search(apple);
         MainPage.listOfCategories.shouldHave(CollectionCondition.size(20));
@@ -53,39 +55,43 @@ public class RozetkaTests {
         MainPage.catalogHeader.shouldHave(Condition.text(apple));
     }
 
-    @Test
+    @Test(description = "Check filters functionality")
     public void test3() {
         MainPage.search(iphone13);
         MainPage.seriesFilters.find(Condition.text("iPhone 13")).shouldHave(Condition.cssClass("checkbox-filter__link--checked"));
         int countOfGoods = MainPage.listOfGoods.size();
         MainPage.sellerFilters.find(Condition.text("Rozetka")).click();
-        MainPage.listOfGoods.shouldHave(CollectionCondition.sizeLessThan(countOfGoods)); // test fails because count of goods not changes
+        MainPage.seriesFilters.find(Condition.text("iPhone 14")).click();
+        MainPage.listOfGoods.shouldBe(CollectionCondition.sizeGreaterThan(countOfGoods));
     }
 
-    @Test
+    @Test(description = "Check size switcher of goods")
     public void test4() throws InterruptedException {
         MainPage.search(iphone13);
-        Thread.sleep(5000);
-        int height = MainPage.listOfGoods.get(0).getSize().getHeight();
-        int width = MainPage.listOfGoods.get(0).getSize().getWidth();
-        Assert.assertEquals(height, MainPage.listOfGoods.get(0).getSize().getHeight());
-        Assert.assertEquals(width, MainPage.listOfGoods.get(0).getSize().getWidth());
+        checkListOfGoodsLoaded();
+        int smallGoodsheight = 455;
+        int smallGoodswidth = 207;
+        Assert.assertEquals(smallGoodsheight, MainPage.listOfGoods.get(0).getSize().getHeight(), "height not equals");
+        Assert.assertEquals(smallGoodswidth, MainPage.listOfGoods.get(0).getSize().getWidth(), "width not equals");
         MainPage.bigTileButton.click();
-        Thread.sleep(5000);
-        height = MainPage.listOfGoods.get(0).getSize().getHeight();
-        width = MainPage.listOfGoods.get(0).getSize().getWidth();
-        Assert.assertEquals(height, MainPage.listOfGoods.get(0).getSize().getHeight());
-        Assert.assertEquals(width, MainPage.listOfGoods.get(0).getSize().getWidth());
+        MainPage.bigTileButton.shouldHave(Condition.cssClass("catalog-view__button_state_active"));
+        int bigGoodsheight = 523;
+        int bigGoodswidth = 259;
+        Assert.assertEquals(bigGoodsheight, MainPage.listOfGoods.get(0).getSize().getHeight(), "height not equals");
+        Assert.assertEquals(bigGoodswidth, MainPage.listOfGoods.get(0).getSize().getWidth(), "width not equals");
     }
 
-    @Test
+    @Test(description = "Check sorting functionality")
     public void test5() throws InterruptedException {
         MainPage.search(iphone);
-        Thread.sleep(8000);
-        MainPage.sorting.selectOptionByValue("2: expensive");
-        Thread.sleep(8000);
-        int firstProductValue = Integer.parseInt(MainPage.productPrice.get(0).getText().replaceAll(" ", "").replaceAll("₴", ""));
-        int secondProductValue = Integer.parseInt(MainPage.productPrice.get(1).getText().replaceAll(" ", "").replaceAll("₴", ""));
-        Assert.assertTrue(firstProductValue > secondProductValue);
+        checkListOfGoodsLoaded();
+        MainPage.sorting.shouldBe(Condition.value("4: rank"));
+        MainPage.sorting.selectOptionByValue(sortingValue);
+        MainPage.sorting.shouldBe(Condition.value(sortingValue));
+        MainPage.producerFilters.find(Condition.text(apple)).shouldBe(Condition.visible);
+        MainPage.producerFilters.find(Condition.text(apple)).shouldHave(Condition.cssClass("checkbox-filter__link--checked"));
+        int firstProductValue = Integer.parseInt(MainPage.productPrice.get(0).getText().replaceAll("[^0-9]", ""));
+        int secondProductValue = Integer.parseInt(MainPage.productPrice.get(1).getText().replaceAll("[^0-9]", ""));
+        Assert.assertTrue(firstProductValue > secondProductValue, "first product price not bigger then second product price");
     }
 }
